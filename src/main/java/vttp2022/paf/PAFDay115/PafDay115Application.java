907a6c7e711d22c46288b6cebdc36e3a101edc39
MichelleHoZY <hozhiying@outlook.com>
2022-04-05 11:06:36 +0800

    initial commit

---
+++ src/main/java/vttp2022/paf/PAFDay115/PafDay115Application.java
@@ -0,0 +1,13 @@
+package vttp2022.paf.PAFDay115;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay115Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay115Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/paf/PAFDay115/PafDay115ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.paf.PAFDay115;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay115ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
